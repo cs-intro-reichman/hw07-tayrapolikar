f72bd1f48d6f07e366ec6e084dd7df8394022d5d
tayrapolikar <tayra.polikar@post.idc.ac.il>
2024-01-30 19:04:31 +0200

    test 1

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -3,38 +3,46 @@
 public class HashTagTokenizer {
 
 	public static void main(String[] args) {
-
 		String hashTag = args[0];
-		String []dictionary = readDictionary("dictionary.txt");
-		breakHashTag(hashTag, dictionary);
+		String[] dictionary = readDictionary("dictionary.txt");
+		breakHashTag(hashTag ,dictionary);
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
-
-		// Your code here
-
+		for (int i = 0; i < dictionary.length; i++) {
+			dictionary[i] = in.readString();
+		}
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		for (int i = 0; i < dictionary.length; i++) {
+			if(dictionary[i].equals(word)){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
+		hashtag = hashtag.toLowerCase();
 
 		// Base case: do nothing (return) if hashtag is an empty string.
-        if (hashtag.isEmpty()) {
-            return;
-        }
- 
-        int N = hashtag.length();
+		if (hashtag.isEmpty()) {
+			return;
+		}
 
-        for (int i = 1; i <= N; i++) {
-		
-        }
-    }
+		int N = hashtag.length();
+
+		for (int i = 1; i <= N; i++) {
+			String pixel = hashtag.substring(0, i);
+			if (existInDictionary(pixel, dictionary)) {
+				System.out.println(pixel);
+				breakHashTag(hashtag.substring(i), dictionary);
+			}
+		}
+	}
 
 }
